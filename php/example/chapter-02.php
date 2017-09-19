16f29905ee5d7fafd9b908280aa1d67a0f5bac0f
nuttakrantraiukhot <31967424+nuttakrantraiukhot@users.noreply.github.com>
2017-09-19 22:06:18 +0700

    Update chapter-02.php

--- php/example/chapter-02.php
+++ php/example/chapter-02.php
@@ -2,12 +2,12 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4........3cff0';
-$access_token  = '2og9ogezC..... W5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '179dab49e9373838009df61ae1684de5';
+$access_token  = '/jlPwu9B9zHK3uZu+NlIRAoTqhYISpzevh1qeAVyqNKDVpM5X7+GtW3aK1gdB6k96Fkd14LElyqGZ2y1Ln59AY2/F6+0jVtmsIyUFArhRmB7ui1WcYNAEDNYwslg4UvDVxz/fhKLSxZFOmLah1gaTAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
-$bot->sendMessageNew('U39f72cc.....d460d6ddf', 'Hello World !!');
+$bot->sendMessageNew('Uc86e42f9f17d087f2c6ca6fae436028c', 'Hello World !!');
 
 if ($bot->isSuccess()) {
     echo 'Succeeded!';
